4b342ffa6ec72e305042d1df521250f12f023937
Marte <marte.mettenes@makingwaves.com>
2019-06-21 10:57:46 +0200

    develop: Add config file

---
+++ src/config.js
@@ -0,0 +1,8 @@
+var config = {
+    apiKey: "AIzaSyC7RYO3XtOo99o_f2c9MjQaiLXo_BujaUY",
+    authDomain: "react-test-mm.firebaseapp.com",
+    databaseURL: "https://react-test-mm.firebaseio.com",
+    projectId: "react-test-mm",
+    storageBucket: "react-test-mm.appspot.com",
+    messagingSenderId: "514154825930"
+};
